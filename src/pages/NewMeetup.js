a1cc24f004cf205046fa6d10746feda46e106640
namanjo <naman14joshi@gmail.com>
2021-08-31 22:51:11 +0530

    Added Firebase API for data
    The Firebase API has now been implemented for rendering data.  We can add data to Firebase database with 'post' request & can render all data in via 'get'. The components are also refactored accordingly.

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,31 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm"
+import { useHistory } from 'react-router-dom';
+
+
 
 function NewMeetup() {
+
+    const history = useHistory();
+
+    function addMeetupHandler(meetupData) {
+        fetch(
+          'https://react-meetup-adae6-default-rtdb.firebaseio.com/meetups.json',
+          {
+            method: 'POST',
+            body: JSON.stringify(meetupData),
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          }
+        ).then(() => {
+            history.replace('/');
+          });
+      }
+
     return (
         <section>
             <h1>New Meetup Page</h1>
-            <NewMeetupForm />
+            <NewMeetupForm onAddMeetup={addMeetupHandler} />
         </section>
         
     )
