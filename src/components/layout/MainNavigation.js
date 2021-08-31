49bd0ce437cdd9968dcbfd5c84abeb58227ef10e
namanjo <naman14joshi@gmail.com>
2021-09-01 00:10:36 +0530

    Implemented Context API for favorite meetup

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,9 +1,14 @@
 import { Link } from "react-router-dom"
-
+import { useContext } from "react";
 import classes from './MainNavigation.module.css'
+
+import FavoritesContext from '../../store/favorites-context';
 
 
 function MainNavigation() {
+
+    const favoritesCtx = useContext(FavoritesContext);
+
     return (
         <header className={classes.header}>
             <div className={classes.logo}>React Meetups</div>
@@ -16,7 +21,12 @@ function MainNavigation() {
                         <Link to='/new-meetup'>Add New Meetup</Link>
                     </li>
                     <li>
-                        <Link to='/favorites'>Favorites</Link>
+                        <Link to='/favorites'>
+                            Favorites
+                            <span className={classes.badge}>
+                                {favoritesCtx.totalFavorites}
+                            </span>
+                        </Link>
                     </li>
                 </ul>
             </nav>
